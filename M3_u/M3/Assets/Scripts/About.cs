e1092325a991360f66fa7e653bd579dbaa5da85a
FedorVL <fedorvlvl@gmail.com>
2019-10-01 15:22:47 +0300

    save

---
+++ M3_u/M3/Assets/Scripts/About.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class About : MonoBehaviour
+{
+
+}
+/* Much3 games
+ https://youtu.be/cqJ5b5aFo5U       5:34
+ * */
