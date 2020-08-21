2e4691de9e80e4dfae536dfc4dec3419d5be4e55
FedorVL <fedorvlvl@gmail.com>
2020-08-21 14:03:38 +0300

    save

---
+++ M3_u/M3/Assets/myM3/description.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class description : MonoBehaviour
+{
+
+}
+
+/*
+ * baseElement - properties: 1) move slote to slote 2) start -> appear from startSlot 3) destroy in slote 4) play animation (appear, idle, destroy, tapOn, hint)
+ * busterElement - properties: 1) move slote to slote 2) start -> appear from special condition 3) destroy in slote 4) play animation (appear, idle, destroy, tapOn, hint)
+ * 
+ * algoritm for much - when element have  (afer move) finish position, he looking arraund himself and if in order or column more then 3 elements - happens destroy this elements
+ */
---
+++ M3_u/M3/Assets/myM3/element.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class element : MonoBehaviour
+{
+    public GameObject Element;
+    public enum ElementsClass {baseElement, busterElement, collectElement, stopElement};
+
+
+    public void MoveSlotToSlot()
+    {
+        
+    }
+}
+
